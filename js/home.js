0c2073b768aac287d9b1442cd8c456cb30163a0f
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2024-08-10 16:33:26 +0700

    fix: homepage

--- js/home.js
+++ js/home.js
@@ -3,46 +3,48 @@ const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 const pageButtons = document.querySelectorAll('.page-btn');
 
-const profilesPerPage = 6;
+let profilesPerPage = 16;
 let currentPage = 1;
+let profiles = [];
+let searchVal = '';
 
-// Sample data (replace with your actual data)
-const profiles = [
-  { name: 'John Doe', nim: '12345', avatar: 'male-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'raykal', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
-  { name: 'Jane Smith', nim: '23456', avatar: 'female-avatar.png' },
+function loadData() {
+  fetch('/database/alumni-2020.json', {
+    method: 'GET',
+  })
+    .then((response) => {
+      response.json().then((jsonResponse) => {
+        // console.log(jsonResponse);
+        profiles = jsonResponse;
+        if (searchVal && searchVal !== '') {
+          profiles = profiles.filter((el) => {
+            const c1 = el.name.toLocaleLowerCase().includes(searchVal);
+            const nim = el.nim.toString().includes(searchVal);
+            const year = el.angkatan + 3 == searchVal;
 
-  // Add more profiles here...
-];
+            return c1 || nim || year;
+          });
+        }
+        displayProfiles(currentPage);
+        updatePagination();
+      });
+      // assuming your json object is wrapped in an array
+      response.json().then((i) => i.forEach((i) => console.log(i.name)));
+    })
+    .catch((err) => {
+      console.log(`Error: ${err}`);
+    });
+}
+
+const form = document.querySelector('#searchForm');
+const searchInput = document.querySelector('#searchQuery');
+form.onsubmit = (e) => {
+  e.preventDefault();
+  const val = searchInput.value;
+  searchVal = val;
+
+  loadData();
+};
 
 function displayProfiles(page) {
   const startIndex = (page - 1) * profilesPerPage;
@@ -53,11 +55,18 @@ function displayProfiles(page) {
 
   pageProfiles.forEach((profile) => {
     const card = document.createElement('div');
+    let img = profile.image;
+    if (!img.includes('.')) {
+      img += '.png';
+    }
+
+    let angkatan = profile.angkatan + 3;
     card.className = 'profile-card';
     card.innerHTML = `
-            <img src="${profile.avatar}" alt="${profile.name}">
+            <img src="img/alumni/${img}" alt="${profile.name}">
             <p class="name">${profile.name}</p>
             <p class="nim">${profile.nim}</p>
+            <p class="angkatan">Lulus tahun ${angkatan}</p>
         `;
     profileGrid.appendChild(card);
   });
@@ -96,6 +105,4 @@ pageButtons.forEach((btn) => {
   });
 });
 
-// Initial display
-displayProfiles(currentPage);
-updatePagination();
+loadData();
